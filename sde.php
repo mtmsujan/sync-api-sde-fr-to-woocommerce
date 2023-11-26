099b1d016424ac2dae9829542e1d365068d91e2e
Md Toriqul Mowla <sujan@Mds-MacBook-Pro.local>
2023-11-27 01:35:47 +0600

    directory structure updated

--- sde.php
+++ sde.php
@@ -15,19 +15,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // create and delete sync_products table on plugin activation and deactivation
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-products-db.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-products-db.php' );
 register_activation_hook( __FILE__, 'sde_sync_products_table' );
 register_deactivation_hook( __FILE__, 'sde_sync_products_table_remove' );
 
 // create and delete sync_orders table on plugin activation and deactivation
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-orders-db.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-orders-db.php' );
 register_activation_hook( __FILE__, 'sde_sync_orders_table' );
 register_deactivation_hook( __FILE__, 'sde_sync_orders_table_remove' );
 
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-products-shortcode.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-products-api.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-orders-shortcode.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'includes/sde-orders-api.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'codestar-framework/codestar-framework.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'metabox.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'includes/menu-page.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-products-shortcode.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-products-api.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-orders-shortcode.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/sde-orders-api.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/cf-metabox/codestar-framework.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/metabox.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'inc/menu-page.php' );
